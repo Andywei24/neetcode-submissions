18c1ad6af48f1fc881161c49562750ee522dd887
Andywei24 <73680510+Andywei24@users.noreply.github.com>
2026-05-15 23:56:12 +0200

    Add: count-connected-components - submission-2

---
+++ Data Structures & Algorithms/count-connected-components/submission-2.py
@@ -0,0 +1,21 @@
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        hashMap = {i:[] for i in range(n)}
+        for node, child in edges:
+            hashMap[node].append(child)
+            hashMap[child].append(node)
+        visited = [False] * n
+        def dfs(node):
+            for nei in hashMap[node]:
+                if not visited[nei]:
+                    visited[nei] = True
+                    dfs(nei)
+        
+        res = 0
+        for node in range(n):
+            if not visited[node]:
+                visited[node] = True
+                dfs(node)
+                res += 1
+        
+        return res
